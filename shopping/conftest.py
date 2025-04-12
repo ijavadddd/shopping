083a229fc952d3ccb31f5b720520c9e538bbbb94
ijavadddd <ijavadd.dev@gmail.com>
2025-04-12 09:15:41 +0330

    initialize project

---
+++ shopping/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from shopping.users.models import User
+from shopping.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def _media_storage(settings, tmpdir) -> None:
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
